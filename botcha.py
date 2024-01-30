06ec407f10424f09a46ef5d964df3901c5313045
hulacorn <behzodxidirov@gmail.com>
2024-01-30 11:36:27 +0500

    fixed issues

--- botcha.py
+++ botcha.py
@@ -57,13 +57,13 @@ def log_user_message(user, message):
 
 # Define a few command handlers. These usually take the two arguments update and
 # context.
-async def start(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
+async def start(update: Update) -> None:
     """Send a message when the command /start is issued."""
     user = update.effective_user
-    await update.message.reply_html((rf"Hi {user.mention_html()}cek!"), reply_markup=buttons)
+    await update.message.reply_html(rf"Hi {user.mention_html()}cek!", reply_markup=buttons)
 
 
-async def help_command(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
+async def help_command(update: Update) -> None:
     """Send a message when the command /help is issued."""
     await update.message.reply_text("Help!")
 
@@ -87,14 +87,14 @@ async def button_handler(update: Update, context: ContextTypes.DEFAULT_TYPE) -> 
 
 def main() -> None:
     """Start the bot."""
-    # Create the Application and pass it your bot's token.
+    # Create the Application and pass it your bots token.
     application = Application.builder().token(TELEGRAM_BOT_TOKEN).build()
 
     # on different commands - answer in Telegram
     application.add_handler(CommandHandler("start", start))
     application.add_handler(CommandHandler("help", help_command))
 
-    # on non command i.e message - echo the message on Telegram
+    # on non command i.e. message - echo the message on Telegram
     application.add_handler(MessageHandler(filters.TEXT & ~filters.COMMAND, button_handler))
 
     # Run the bot until the user presses Ctrl-C
