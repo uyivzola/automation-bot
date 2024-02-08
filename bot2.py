295bfcd3c4082b5cf5591138f6808dab981f6413
hulacorn <behzodxidirov@gmail.com>
2024-02-08 23:25:19 +0500

    started working with conversation.py and conversation handler

--- bot2.py
+++ bot2.py
@@ -1,244 +1,184 @@
-# -*- coding: UTF-8 -*-
+import logging
 import os
-from datetime import datetime
 
+from openpyxl.worksheet.filters import Filters
+from telegram import ReplyKeyboardMarkup, ReplyKeyboardRemove, Update, InlineKeyboardButton, InlineKeyboardMarkup, \
+    KeyboardButton
+from telegram.ext import (Application, CommandHandler, ContextTypes, ConversationHandler, MessageHandler, filters,
+                          CallbackContext, Updater)
 from dotenv import load_dotenv
-from telegram import Update, ReplyKeyboardMarkup
-from telegram.ext import ApplicationBuilder, CommandHandler, ContextTypes, MessageHandler, filters
-
-from reports.limit import limit_generator
-from reports.oxvat import oxvat_generator
-from reports.top import top_generator
 
 load_dotenv()
-TELEGRAM_BOT_TOKEN = os.getenv("TELEGRAM_BOT_TOKEN2")
-BOT_USERNAME = os.getenv("BOT_USERNAME2")
+TELEGRAM_BOT_TOKEN = os.getenv('TELEGRAM_BOT_TOKEN')
+BOT_USERNAME = os.getenv('BOT_USERNAME')
+
+# Enable logging
+logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s', level=logging.INFO)
+# set higher logging level for httpx to avoid all GET and POST requests being logged
+logging.getLogger('httpx').setLevel(logging.WARNING)
+logger = logging.getLogger(__name__)
+
+GENDER, PHOTO, LOCATION, PHONE_NUMBER, DELPHI_LOGIN, DELPHI_PASSWORD = range(6)
 
 
-def log_user_message(user, message):
-    # Get current timestamp
-    timestamp = datetime.now().strftime('%Y-%m-%d %H:%M:%S')
+async def start(update: Update, context: ContextTypes.DEFAULT_TYPE) -> int:
+    """ Starts the conversation and ask the user about their gender"""
+    reply_keyboard = [['Boy', 'Girl', 'Other']]
+    # reply_keyboard_inline = [[InlineKeyboardButton('Boy', callback_data=print('pressed 1'))]]
+    # reply_keyboard_inline_markup = InlineKeyboardMarkup(reply_keyboard_inline)
 
-    # Format the log message
-    log_message = (f'{timestamp} - UserID: {user.id}, '
-                   f'User: {user.username}, '
-                   f'First Name: {user.first_name}, '
-                   f'Message: {message}\n')
-
-    # Write the log message to a file
-    with open('log.txt', 'a') as log_file:
-        log_file.write(log_message)
+    reply_markup = ReplyKeyboardMarkup(reply_keyboard, one_time_keyboard=True, input_field_placeholder="Qanaqasan?")
+    await update.message.reply_text("HI! My name is Professor Behzod Khidirov! I will hold a conversation with you! "
+                                    "Are you Boy or a girl?", reply_markup=reply_markup)
+    return GENDER
 
 
-def handle_response(text: str) -> str:
-    processed: str = text.lower()
+async def gender(update: Update, context: ContextTypes.DEFAULT_TYPE) -> int:
+    """Stores selected gender and asks for a photo of the user"""
+    user = update.message.from_user.first_name
+    gender = update.message.text
 
-    if 'hello' in processed:
-        return 'Hello there!'
+    logger.info("Gender of %s: %s", user, gender)
+    context.user_data['gender'] = gender
+    await update.message.reply_text(
+        "I see! Please send me a photo of yourself..."
+        "so I know what you look like", reply_markup=ReplyKeyboardRemove(),
+    )
 
-    if 'how are you' in processed:
-        return 'Remember to subscribe!'
+    return PHOTO
 
-buttons=ReplyKeyboardMarkup([['Statistica'],['Dunyo']])
-def store(update,context):
-    update.message.reply_text(f'Hello, {update.message.from_user.first_name}!', reply_markup=buttons)
-async def handle_message(update: Update, context: ContextTypes.DEFAULT_TYPE):
-    message_text: str = update.message.text
+
+location_reply_keyboard = [
+    [KeyboardButton("Send Location 📍", request_location=True)]
+]
+location_markup = ReplyKeyboardMarkup(location_reply_keyboard, one_time_keyboard=True,
+                                      input_field_placeholder='Manzilingizni kiriting.')
+
+
+async def photo(update: Update, context: ContextTypes.DEFAULT_TYPE) -> int:
+    """Stores the photo and asks for a location."""
     user = update.message.from_user
+    picture = update.message.photo[-1]  # Assuming the last photo is the largest
+    picture_id = picture.file_id
+    photo_file = await update.message.photo[-1].get_file()
+    context.user_data['picture_id'] = picture_id
 
-    # Log the user message
-    log_user_message(user, message_text)
+    await photo_file.download_to_drive(f'{user.first_name}s photo.jpg')
 
-    print(f'User:({update.message.chat.id}, {user}) - "{message_text}"')
-
-    if 'limit' in message_text.lower():
-        await limit(update, context)
-    if 'top' in message_text.lower():
-        await top(update, context)
-    if 'oxvat' in message_text.lower():
-        await oxvat(update, context)
-    if 'start' in message_text.lower():
-        await started(update, context)
-    else:
-        response: str = handle_response(message_text)
-        print('Bot:', response)
-        await update.message.reply_text(response)
+    logger.info("Photo of %s: %s", user.first_name, "photo is stored as it is")
+    await update.message.reply_text("you are so cute!"
+                                    "please give me your location", reply_markup=location_markup)
+    return LOCATION
 
 
-async def error(update: Update, context: ContextTypes.DEFAULT_TYPE):
-    print(f'Update {update} caused error {context.error}')
+phone_reply_keyboard = [
+    [KeyboardButton("Send Phone Number ☎️", request_contact=True)]
+]
+
+phone_markup = ReplyKeyboardMarkup(phone_reply_keyboard, one_time_keyboard=True,
+                                   input_field_placeholder='Telefon raqamingizni yuboring')
 
 
-# Sends LIMIT CLIENTS
-async def limit(update, context):
+async def location(update: Update, context: ContextTypes.DEFAULT_TYPE) -> int:
+    """Stores the location and asks for phone number info about the user"""
     user = update.message.from_user
-    username = user.first_name
+    user_location = update.message.location
+    latitude, longitude = user_location.latitude, user_location.longitude
 
-    today_date = datetime.now().strftime('%d %b')
-    file_name = f'LIMIT - {today_date}.xlsx'
+    # Assuming your chat_id is stored in the variable 'chat_id'
     chat_id = update.message.chat_id
+    context.user_data["location"] = f"Latitude: {latitude}, Longitude: {longitude}"
+    logger.info("Contact of %s: %s", user.first_name, user_location)
 
-    # Send a preliminary message
-    message = await update.message.reply_text(
-        f"Sizning so\'rovingiz bo\'yicha  \n *LIMIT \- {today_date}\.xlsx* \n fayl tayyorlanmoqda😎 "
-        "Iltimos kuting⌛⌛⌛ \(o\'rtacha 5 daqiqa\)", parse_mode='MarkdownV2')
+    await update.message.reply_text(f"Thanks for sharing your location! {context.user_data['location']}"
+                                    f"Now I need your phone number!", reply_markup=phone_markup)
 
-    try:
-
-        limit_generator()
-        # Check the modification time of the file
-        modification_time = datetime.fromtimestamp(os.path.getmtime(file_name))
-
-        # Open and send the document
-        document = open(file_name, 'rb')
-        await context.bot.send_document(chat_id, document,
-                                        caption=f"Savdoyingizga baraka tilayman🤲🏼💸, Hurmatli {username}!😻\n \n\n"
-                                                f"Ushbu fayl {modification_time.strftime('%d-%B, soat %H:%M')} da "
-                                                f"yangilangan.")
-
-        # Delete the preliminary message
-        await message.delete()
-
-        # Send a final message
-        spoiler_text = ("|| Bu bot test rejimida ishlamoqda\!"
-                        " Xatolar bo\'lsa to\'g\'ri tushunasiz degan umiddaman😊 ||")
-
-        await update.message.reply_text(spoiler_text, parse_mode='MarkdownV2')
-    except Exception as e:
-        # Handle exceptions and reply with an error message
-        error_message = f'Error sending the file: {str(e)}'
-        print(error_message)
-        await update.message.reply_text(error_message)
+    return PHONE_NUMBER
 
 
-# Sends TOP OSTATOK TOVAROV
-async def top(update, context):
+async def phone_number(update: Update, context: ContextTypes.DEFAULT_TYPE) -> int:
+    """Stores the phone number and asks for user his/her login to Delphi"""
     user = update.message.from_user
-    username = user.first_name
+    contact = update.message.contact
+    context.user_data["phone_number"] = contact
+    logger.info("Contact of %s: %s", user.first_name, contact)
 
-    today_date = datetime.now().strftime('%d %b')
-    file_name = f'TOP ostatok - {today_date}.xlsx'
+    await update.message.reply_text(f"Good. Please enter your login to DELPHI: ", reply_markup=ReplyKeyboardRemove()
+                                    )
+    return DELPHI_LOGIN
+
+
+async def delphi_login(update: Update, context: ContextTypes.DEFAULT_TYPE) -> int:
+    """Stores the Delphi login and asks for user his/her PASSWORD to Delphi"""
+    user = update.message.from_user
+    message_id = update.message.message_id
+    context.user_data["login"] = update.message.text
+    await update.message.reply_text(f"Good user with login: {update.message.text}"
+                                    f"Please enter your password:")
+    logger.info("Login of %s: %s", user.first_name, update.message.text)
+
+    return DELPHI_PASSWORD
+
+
+async def delphi_password(update: Update, context: ContextTypes.DEFAULT_TYPE) -> int:
+    """Stores the Delphi Password and asks for user his/her shit"""
+    user = update.message.from_user
     chat_id = update.message.chat_id
+    message_id = update.message.message_id
+    password = update.message.text
+    if update.message.chat.type != 'private':
+        await update.message.reply_text('Please send your password in a private chat for security.')
+        return ConversationHandler.END
 
-    # Send a preliminary message
-    message = await update.message.reply_text(
-        f"Sizning so\'rovingiz bo\'yicha \n *TOP ostatok \- {today_date}\.xlsx* \nfayl tayyorlanmoqda😎 "
-        "Iltimos kuting⌛\(o\'rtacha 3 daqiqa\)", parse_mode='MarkdownV2')
+    # Store the password securely in the user_data
+    context.user_data["password"] = password
 
-    try:
+    # Delete the password message from the user's perspective
+    await context.bot.deleteMessage(chat_id=chat_id, message_id=message_id)
 
-        top_generator()
-        # Check the modification time of the file
-        modification_time = datetime.fromtimestamp(os.path.getmtime(file_name))
+    # Perform further actions based on your application's logic
+    # PASSWORD IS HERE and it is stored as password
 
-        # Open and send the document
-        document = open(file_name, 'rb')
-        await context.bot.send_document(chat_id, document,
-                                        caption=f"Asklepiy Distribution Skladidagi Tovarlar ro'yxati📑. \n Charchamang, {username}!😉\n \n\n"
-                                                f"Ushbu fayl {modification_time.strftime('%d-%B, soat %H:%M')}da "
-                                                f"yangilangan.")
+    await update.message.reply_text('See you soon! Good bye!')
 
-        # Delete the preliminary message
-        await message.delete()
-
-        # Send a final message
-        spoiler_text = ("|| Bu bot test rejimida ishlamoqda\!"
-                        " Xatolar bo\'lsa to\'g\'ri tushunasiz degan umiddaman😊 ||")
-
-        await update.message.reply_text(spoiler_text, parse_mode='MarkdownV2')
-    except Exception as e:
-        # Handle exceptions and reply with an error message
-        error_message = f'Error sending the file: {str(e)}'
-        print(error_message)
-        await update.message.reply_text(error_message)
+    logger.info("Password of %s: %s", user.first_name, update.message.text)
+    await update.message.reply_text('Password received. Processing your request securely 🔐')
+    return ConversationHandler.END
 
 
-# OXVAT RUN sends oxvachenniye klienti!
-async def oxvat(update, context):
+async def cancel(update: Update, context: ContextTypes.DEFAULT_TYPE) -> int:
+    """Cancels and ends the conversation with the user"""
     user = update.message.from_user
-    username = user.first_name
+    logger.info("User %s canceled the conversation.", user.first_name)
+    await update.message.reply_text("Bye! I hope we can talk again some day.", reply_markup=ReplyKeyboardRemove())
 
-    today_date = datetime.now().strftime('%d %b')
-    file_name = f'NE OXVACHEN - {today_date}.xlsx'
-    chat_id = update.message.chat_id
-
-    # Send a preliminary message
-    message = await update.message.reply_text(
-        f"Sizning so\'rovingiz bo\'yicha \n  *NE OXVACHEN \- {today_date}\.xlsx*  \n fayl tayyorlanmoqda😎 "
-        "Iltimos kuting⌛⌛⌛\(o\'rtacha 5 daqiqa\)", parse_mode='MarkdownV2')
-
-    try:
-
-        oxvat_generator()
-        # Check the modification time of the file
-        modification_time = datetime.fromtimestamp(os.path.getmtime(file_name))
-
-        # Open and send the document
-        document = open(file_name, 'rb')
-        await context.bot.send_document(chat_id, document, caption=f"Yangi mijozlar bilan xushmomila bo'ling🙈\n \n\n"
-                                                                   f"Ushbu fayl {modification_time.strftime('%d-%B, soat %H:%M')} da "
-                                                                   f"yangilangan.")
-
-        # Delete the preliminary message
-        await message.delete()
-
-        # Send a final message
-        spoiler_text = ("|| Bu bot test rejimida ishlamoqda\!"
-                        " Xatolar bo\'lsa to\'g\'ri tushunasiz degan umiddaman😊 ||")
-
-        await update.message.reply_text(spoiler_text, parse_mode='MarkdownV2')
-    except Exception as e:
-        # Handle exceptions and reply with an error message
-        error_message = f'Error sending the file: {str(e)}'
-        print(error_message)
-        await update.message.reply_text(error_message)
+    return ConversationHandler.END
 
 
-## Starting text where it will welcome the users and introduce what this bot can do.
-async def started(update, context):
-    user = update.message.from_user
-    username = user.first_name
+def main() -> None:
+    """RUN THE BOT"""
+    # Create the Application and pass it your bot's token
 
-    today_date = datetime.now().strftime('%d %b')
+    application = Application.builder().token(TELEGRAM_BOT_TOKEN).build()
 
-    # Send a preliminary message
-    message = await update.message.reply_text(f"""
-Assalomu alaykum! Mening botimga xush kelibsiz, {username}!
-Ushbu bot sizga LIMIT | OXVAT | TOP ostatok kabi ro'yxatlarni yuboradi.
+    # Add conversation handler with the states GENDER, PHOTO, LOCATION, PHONE_NUMBER, DELPHI_LOGIN, DELPHI_PASSWORD
 
-Sizga bot tomonidan fayl kelmagunigacha ozroq kuting. ⏳🔁 (o'rtacha 5 daqiqa)
+    conv_handler = ConversationHandler(
+        entry_points=[CommandHandler('start', start)],
+        states={
+            GENDER: [MessageHandler(filters.Regex("^(Boy|Girl|Other$)"), gender)],
+            PHOTO: [MessageHandler(filters.PHOTO, photo)],
+            LOCATION: [MessageHandler(filters._Location(), location)],
+            PHONE_NUMBER: [MessageHandler(filters._Contact(), phone_number)],
+            DELPHI_LOGIN: [MessageHandler(filters.TEXT, delphi_login)],
+            DELPHI_PASSWORD: [MessageHandler(filters.TEXT, delphi_password)]
+        }, fallbacks=[CommandHandler("cancel", cancel)]
+    )
 
-Shunchaki quyidagilarni yozing📈:
-- oxvat yoki /oxvat
-- limit yoki /limit
-- top yoki /top
-
-Qo'llab quvvatlasangiz, juda ham hursand bo'laman!🥰
-
-Hurmat bilan Behzod🖤 . 
-
-Murojaat uchun @hopxol
-""")
+    application.add_handler(conv_handler)
+    # Run the bot until the user presses CTRL - C
+    application.run_polling(allowed_updates=Update.ALL_TYPES)
 
 
 if __name__ == '__main__':
-    print('🤖 Starting bot - 2...')
-    app = ApplicationBuilder().token(TELEGRAM_BOT_TOKEN).build()
-
-    # Messages
-    app.add_handler(MessageHandler(filters.TEXT, handle_message))
-
-    # Commands
-    app.add_handler(CommandHandler("start", started))
-
-    app.add_handler(CommandHandler("limit", limit))
-    app.add_handler(CommandHandler("top", top))
-    app.add_handler(CommandHandler("oxvat", oxvat))
-    app.add_handler(CommandHandler("store", store))
-
-    # Error handler
-    app.add_error_handler(error)
-
-    # Polls the bot
-    print('Polling...')
-    app.run_polling(poll_interval=1)
+    main()
