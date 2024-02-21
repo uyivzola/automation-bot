b74faf0eaf22c847cbc1148b3667be36138bd71c
uyivzola <behzodxidirov@gmail.com>
2024-02-21 16:11:39 +0500

    minor changes

--- botcha.py
+++ botcha.py
@@ -1,19 +1,3 @@
-#!/usr/bin/env python
-# pylint: disable=unused-argument
-# This program is dedicated to the public domain under the CC0 license.
-
-"""
-Simple Bot to reply to Telegram messages.
-
-First, a few handler functions are defined. Then, those functions are passed to
-the Application and registered at their respective places.
-Then, the bot is started and runs until we press Ctrl-C on the command line.
-
-Usage:
-Basic Echobot example, repeats messages.
-Press Ctrl-C on the command line or send a signal to the process to stop the
-bot.
-"""
 import asyncio
 import concurrent.futures
 import logging
